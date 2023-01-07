2a0d52b68cf5f8f8d1626398ee78882e91cb6f11
Omkar Kesarkhane <omkesarkhane@gmail.com>
2023-01-07 22:57:15 +0530

    ADD : added theme colors and color root variables

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,4 +4,13 @@ import Vuetify from 'vuetify/lib/framework';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    theme: {
+        themes: {
+            light: {
+                primary: "#6C00FF",
+                secondary: "#3C79F5",
+                tertiary: "#F2DEBA"
+            }
+        }
+    }
 });
