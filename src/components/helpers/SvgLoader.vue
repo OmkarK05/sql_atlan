a31d479ed8eb0c0615062179df9ea326d44d4266
Omkar Kesarkhane <omkesarkhane@gmail.com>
2023-01-07 19:56:24 +0530

    ADD: svg loader component to render svgs with required fill, width and height

---
+++ src/components/helpers/SvgLoader.vue
@@ -0,0 +1,48 @@
+<template lang="">
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    :width="width"
+    :height="height"
+    viewBox="0 0 18 18"
+    :aria-labelledby="iconName"
+    role="presentation"
+  >
+    <title
+      :id="id"
+      lang="en"
+    >{{ iconName }} icon</title>
+    <g :fill="color">
+      <slot />
+    </g>
+  </svg>
+</template>
+<script>
+export default {
+    props: {
+        height: {
+            type: Number,
+            default: 0
+        },
+        width: {
+            type: Number,
+            default: 0
+        },
+        iconName: {
+            type: String,
+            default: ''
+        },
+        id: {
+            type: String,
+            default: ''
+        },
+        color: {
+           type: String,
+           default: ''
+        }
+    }
+    
+}
+</script>
+<style lang="">
+    
+</style>
