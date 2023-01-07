870e567fbd32d4c16b5047ca37d9189cba2d0232
Omkar Kesarkhane <omkesarkhane@gmail.com>
2023-01-07 19:54:59 +0530

    CORE : added vuetify, eslint and sass

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,21 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" width="25%">
-    <HelloWorld msg="Hello Vue in CodeSandbox!"/>
-  </div>
+  <v-app>
+    <QueryDB />
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld";
+import QueryDB from './components/QueryDB.vue';
 
 export default {
-  name: "App",
+  name: 'App',
+
   components: {
-    HelloWorld
-  }
+    QueryDB
+},
+
+  data: () => ({
+    //
+  }),
 };
 </script>
-
-<style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
