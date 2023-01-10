0a29b94ce915ca146032d387bf4da38d725b8858
Omkar Kesarkhane <omkesarkhane@gmail.com>
2023-01-10 11:15:50 +0530

    ADD: added tabs in sidebar

--- src/components/QuerySidebar.vue
+++ src/components/QuerySidebar.vue
@@ -1,6 +1,9 @@
 <template lang="">
-  <div class="px-3">
-    <div class="query-selector">
+  <div class="px-3 py-3">
+    <div class="__title primary--text">
+      Analytics 
+    </div>
+    <!-- <div class="query-selector">
       <v-col
         class="d-flex"
         cols="12"
@@ -14,12 +17,41 @@
           @change="querySelected"
         />
       </v-col>
+    </div> -->
+    <div class="tabs-container">
+      <div
+        v-for="tab in tabs"
+        :key="tab['name']"
+        class="__tab"
+        :class="tab['active'] ? 'active': ''"
+        @click="switchTab(tab)"
+      >
+        <SvgLoader
+          width="18"
+          height="18"
+          :icon-name="tab['label']"
+          :color="tab['active'] ? '#ffffff': '#6c00ff'"
+          class="mr-2"
+        >
+          <component :is="tab['icon']" />
+        </SvgLoader>
+        {{ tab['label'] }}
+      </div>
     </div>
   </div>
 </template>
 <script>
+import SvgLoader from './helpers/SvgLoader.vue';
+
 export default {
     name: 'QuerySidebar',
+    components: {SvgLoader},
+    props: {
+      tabs: {
+        type: Array,
+        default: () => []
+      }
+    },
     data: function (){
         return {
             selectedQuery: '',
@@ -34,7 +66,7 @@ export default {
                 {name: 'suppliers', label: "Suppliers"},
 
             ],
-            selectedQueryData: null
+            selectedQueryData: null,
         }
     },
     computed: {
@@ -54,14 +86,38 @@ export default {
         }catch (error){
           console.log(error);
         }
+      },
+      switchTab: function (tab) {
+        this.$emit('switch-tab', tab)
       }
     }
 }
 </script>
 <style lang="scss" scoped>
-    .query-selector{
-        .query-selector-menu{
-            width: 100%;
+    .__title{
+      font-size: 24px;
+      color: rgba(var(--secondary), 1);
+      text-align: left;
+      margin-bottom: 20px;
+    }
+    .tabs-container{
+      .__tab{
+        width: 100%;
+        padding: 8px 8px;
+        cursor: pointer;
+        border-radius: 4px;
+        display: flex;
+        align-items: center;
+        margin: 4px 0;
+
+        &.active{
+          color: #ffffff;
+          background-color: rgba(var(--primary), 1) !important
         }
+      
+        &:hover{
+          background-color: rgba(var(--secondary), 0.2);
+        }
+      }
     }
 </style>
