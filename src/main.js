fb801d36459b3f676759d97a085fb60760e58d71
omkar kesarkhane <omkesarkhane@gmail.com>
2023-01-12 02:15:29 +0530

    CORE : added event bus and update vuex in main

--- src/main.js
+++ src/main.js
@@ -1,13 +1,17 @@
-import Vue from 'vue'
-import App from './App.vue'
-import vuetify from './plugins/vuetify'
-import { GlobalMixin } from './mixins/globalMixin'
-import './assets/scss/global.scss'
+import Vue from "vue";
+import App from "./App.vue";
+import vuetify from "./plugins/vuetify";
+import { GlobalMixin } from "./mixins/globalMixin";
+import "./assets/scss/global.scss";
+import Vuex from "vuex";
+import store from "./store";
 
 Vue.config.productionTip = false;
 Vue.mixin(GlobalMixin);
+Vue.use(Vuex);
 
 new Vue({
   vuetify,
-  render: h => h(App)
-}).$mount('#app')
+  store,
+  render: (h) => h(App),
+}).$mount("#app");
