49f4076cb2ed2ff187be7b5de9e8e45043ab4331
omkar kesarkhane <omkesarkhane@gmail.com>
2023-01-11 23:36:59 +0530

    Add : Store for data

---
+++ src/store/components/data.js
@@ -0,0 +1,23 @@
+export const data = {
+  namespaced: true,
+
+  state: {
+    selectedData: {},
+  },
+
+  getters: {
+    getSelectedData: (state) => state.selectedData,
+  },
+
+  mutations: {
+    SET_DATA: function (state, payload) {
+      state.selectedData = payload;
+    },
+  },
+
+  actions: {
+    setSelectedData: function (store, data) {
+      store.commit("SET_DATA", data);
+    },
+  },
+};
